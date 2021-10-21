f161b0ac699541ceece805bb47131a1d735e78c7
shrpne <shrpne@gmail.com>
2021-10-21 20:25:11 +0300

    refactor footer

--- layouts/_footer.vue
+++ layouts/_footer.vue
@@ -33,48 +33,48 @@ export default {
         </section>
         <footer class="footer">
             <div class="container">
-                <div class="footer-disclaimer">
+                <div class="footer__disclaimer">
                     <h3>{{ $td('Disclaimer', 'index.footer-disclaimer-title')}}</h3>
                     <p>{{ $td('Please note that you are accessing the ALPHA Version of the Honee service which is in the process of being tested before its official release. The sole purpose of this ALPHA Version is to conduct testing and obtain feedback.', 'index.footer-disclaimer-p1')}}</p>
                     <p>{{ $td('ALPHA testing is free for users and we do not demand any remuneration for access to the ALPHA Version of the services. Intra-blockchain fees you pay for transactions are not the payment for using the Honee service.', 'index.footer-disclaimer-p2')}}</p>
                     <p>{{ $td('Operations with virtual assets pose intrinsic risks, including the risk of monetary loss. We strongly suggest you seek professional advice before engaging in any operations with virtual assets.', 'index.footer-disclaimer-p3')}}</p>
                     <p>{{ $td('We do not give any warranties, whether express or implied, as to the suitability or usability of the service, its software or any of its content. We will not be liable for any loss, whether such loss is direct, indirect, special or consequential, suffered by any party as a result of their use of the ALPHA, its content and functionalities.', 'index.footer-disclaimer-p4')}}</p>
-                    <p>{{ $td('Should you encounter any bugs, glitches, lack of functionality or other problems on the ALPHA, please email us at', 'index.footer-disclaimer-p5')}} <a href="mailto:hello@honee.app" class="link--underline u-fw-600">hello@honee.app</a>.</p>
+                    <p>{{ $td('Should you encounter any bugs, glitches, lack of functionality or other problems on the ALPHA, please email us at', 'index.footer-disclaimer-p5')}} <a href="mailto:hello@honee.app" class="link--default-black">hello@honee.app</a>.</p>
                 </div>
-                <div class="footer-menu">
-                    <div class="info-links">
-                        <h3>{{ $td('Useful Information', 'index.footer-menu-useful')}}</h3>
-                        <div class="footer-link-box">
-                            <div class="buy-links">
-                                <a :href="$i18nGetPreferredPath('/how-to-buy-bitcoin-2022')">{{ $td('Buy Bitcoin', 'index.footer-link-box-buy-bitcoin')}}</a>
-                                <a :href="$i18nGetPreferredPath('/how-to-buy-ethereum-2022')">{{ $td('Buy Ethereum', 'index.footer-link-box-buy-ethereum')}}</a>
+                <div class="footer__menu u-grid u-grid--medium u-grid--vertical-margin--medium">
+                    <div class="u-cell u-cell--auto">
+                        <h3 class="footer__caption">{{ $td('Useful Information', 'index.footer-menu-useful')}}</h3>
+                        <div class="footer__links-grid u-grid u-grid--vertical-margin--small">
+                            <div class="footer__links-cell u-cell u-cell--auto">
+                                <a class="link--default-black" :href="$i18nGetPreferredPath('/how-to-buy-bitcoin-2022')">{{ $td('Buy Bitcoin', 'index.footer-link-box-buy-bitcoin')}}</a>
+                                <a class="link--default-black" :href="$i18nGetPreferredPath('/how-to-buy-ethereum-2022')">{{ $td('Buy Ethereum', 'index.footer-link-box-buy-ethereum')}}</a>
                             </div>
-                            <div class="earn-links">
-                                <a :href="$i18nGetPreferredPath('/how-to-earn-bitcoin-2022')">{{ $td('Earn Bitcoin', 'index.footer-link-box-earn-bitcoin')}}</a>
-                                <a :href="$i18nGetPreferredPath('/how-to-earn-ethereum-2022')">{{ $td('Earn Ethereum', 'index.footer-link-box-earn-ethereum')}}</a>
+                            <div class="footer__links-cell u-cell u-cell--auto">
+                                <a class="link--default-black" :href="$i18nGetPreferredPath('/how-to-earn-bitcoin-2022')">{{ $td('Earn Bitcoin', 'index.footer-link-box-earn-bitcoin')}}</a>
+                                <a class="link--default-black" :href="$i18nGetPreferredPath('/how-to-earn-ethereum-2022')">{{ $td('Earn Ethereum', 'index.footer-link-box-earn-ethereum')}}</a>
                             </div>
-                            <div class="wallet-links">
-                                <a :href="$i18nGetPreferredPath('/best-bitcoin-wallet-2022')">{{ $td('Bitcoin Wallet', 'index.footer-link-box-wallet-bitcoin')}}</a>
-                                <a :href="$i18nGetPreferredPath('/best-ethereum-wallet-2022')">{{ $td('Ethereum Wallet', 'index.footer-link-box-wallet-ethereum')}}</a>
+                            <div class="footer__links-cell u-cell u-cell--auto">
+                                <a class="link--default-black" :href="$i18nGetPreferredPath('/best-bitcoin-wallet-2022')">{{ $td('Bitcoin Wallet', 'index.footer-link-box-wallet-bitcoin')}}</a>
+                                <a class="link--default-black" :href="$i18nGetPreferredPath('/best-ethereum-wallet-2022')">{{ $td('Ethereum Wallet', 'index.footer-link-box-wallet-ethereum')}}</a>
                             </div>
                         </div>
                     </div>
-                    <div class="about-links">
-                        <h3>{{ $td('About Honee', 'index.footer-menu-about')}}</h3>
-                        <ul>
-                            <li><a href="/legal/terms.pdf" target="_blank">Terms</a></li>
-                            <li><a href="/legal/privacy.pdf" target="_blank">Privacy</a></li>
-                            <li><a href="mailto:hello@honee.app" target="_blank">Contact</a></li>
-                        </ul>
+                    <div class="u-cell u-cell--auto">
+                        <h3 class="footer__caption">{{ $td('About Honee', 'index.footer-menu-about')}}</h3>
+                        <div class="footer__links-cell">
+                            <a class="link--default-black" href="/legal/terms.pdf" target="_blank">Terms</a>
+                            <a class="link--default-black" href="/legal/privacy.pdf" target="_blank">Privacy</a>
+                            <a class="link--default-black" href="mailto:hello@honee.app" target="_blank">Contact</a>
+                        </div>
                     </div>
-                    <div class="footer-contacts">
-                        <h3>{{ $td('Contacts', 'index.footer-menu-contacts')}}</h3>
+                    <div class="footer__contacts u-cell u-cell--auto">
+                        <h3 class="footer__caption">{{ $td('Contacts', 'index.footer-menu-contacts')}}</h3>
                         GROM R&D LIMITED, COMPANY NUMBER 12444209, 27 OLD GLOUCESTER STREET, LONDON, UNITED KINGDOM, WC1N 3AX.<br>
-                        <a href="mailto:hello@honee.app" class="link--underline u-fw-600">hello@honee.app</a>
+                        <a href="mailto:hello@honee.app" class="link--default-black">hello@honee.app</a>
                     </div>
                 </div>
-                <div class="logo"><img src="/img/logo-honee.svg" alt="" width="82" height="16"/></div>
-                <div class="footer-copyright">
+                <nuxt-link class="footer__logo" :to="$i18nGetPreferredPath('/')"><img src="/img/logo-honee.svg" alt="" width="82" height="16"/></nuxt-link>
+                <div class="footer__copyright">
                     Copyright © 2021 Grom R&D Limited.<br>
                     All Rights Reserved.
                 </div>
