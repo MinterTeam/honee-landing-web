8d95e4ad35e471ad28af30fed3b38e6fe379f457
2z2z2z <61245073+2z2z2z@users.noreply.github.com>
2022-11-17 10:02:17 +0300

    ru copytrading

--- pages/copytrading.vue
+++ pages/copytrading.vue
@@ -58,113 +58,135 @@ export default {
         <section class="how">
             <div class="container">
                 <h2>{{ $td('How it Works', 'copytrading.how-title')}}</h2>
-                <div class="how-block">
-                    <div class="how-item">
-                        <div class="how-item-img">
-                            <img src="/img/landing/copytrading/how-1-bg.svg" alt="" class="how-item-bg how-item-1">
-                            <picture>
-                                <source srcset="/img/landing/copytrading/how-1.avif 1x, /img/landing/copytrading/how-1@2x.avif 2x" type="image/avif">
-                                <source srcset="/img/landing/copytrading/how-1.webp 1x, /img/landing/copytrading/how-1@2x.webp 2x" type="image/webp">
-                                <img src="/img/landing/copytrading/how-1.png" srcset="/img/landing/copytrading/how-1@2x.png 2x" alt="" role="presentation"/>
-                            </picture>
-                        </div>
-                        <div>
-                            <template v-if="$i18n.locale === 'en'">
-                                Create and top up <br>a wallet in Honee
-                            </template>
-                            <template v-if="$i18n.locale === 'ru'">
-                                Создайте и пополните <br>кошелек в Honee
-                            </template>
-                        </div>
-                    </div>
-                    <div class="how-item how-item-center">
-                        <div class="how-item-img">
-                            <img src="/img/landing/copytrading/how-2-bg.svg" alt="" class="how-item-bg how-item-2">
-                            <picture>
-                                <source srcset="/img/landing/copytrading/how-2.avif 1x, /img/landing/copytrading/how-2@2x.avif 2x" type="image/avif">
-                                <source srcset="/img/landing/copytrading/how-2.webp 1x, /img/landing/copytrading/how-2@2x.webp 2x" type="image/webp">
-                                <img src="/img/landing/copytrading/how-2.png" srcset="/img/landing/copytrading/how-2@2x.png 2x" alt="" role="presentation"/>
-                            </picture>
-                        </div>
-                        <div>
-                            <template v-if="$i18n.locale === 'en'">
-                                Buy a crypto portfolio <br>managed by experts *
-                            </template>
-                            <template v-if="$i18n.locale === 'ru'">
-                                Купите крипто-портфель, <br>управляемый экспертом *
-                            </template>
-                        </div>
-                    </div>
-                    <div class="how-item">
-                        <div class="how-item-img">
-                            <img src="/img/landing/copytrading/how-3-bg.svg" alt="" class="how-item-bg how-item-3">
-                            <picture>
-                                <source srcset="/img/landing/copytrading/how-3.avif 1x, /img/landing/copytrading/how-3@2x.avif 2x" type="image/avif">
-                                <source srcset="/img/landing/copytrading/how-3.webp 1x, /img/landing/copytrading/how-3@2x.webp 2x" type="image/webp">
-                                <img src="/img/landing/copytrading/how-3.png" srcset="/img/landing/copytrading/how-3@2x.png 2x" alt="" role="presentation"/>
-                            </picture>
-                        </div>
-                        <div>
-                            <template v-if="$i18n.locale === 'en'">
-                                Subscribe to never miss <br>out on portfolio edits
-                            </template>
-                            <template v-if="$i18n.locale === 'ru'">
-                                Подпишитесь на уведомления, <br>чтобы не пропустить изменения портфеля
-                            </template>
-                        </div>
-                    </div>
-                    <div class="how-item how-item-center">
-                        <div class="how-item-img">
-                            <img src="/img/landing/copytrading/how-4-bg.svg" alt="" class="how-item-bg how-item-4">
-                            <picture>
-                                <source srcset="/img/landing/copytrading/how-4.avif 1x, /img/landing/copytrading/how-4@2x.avif 2x" type="image/avif">
-                                <source srcset="/img/landing/copytrading/how-4.webp 1x, /img/landing/copytrading/how-4@2x.webp 2x" type="image/webp">
-                                <img src="/img/landing/copytrading/how-4.png" srcset="/img/landing/copytrading/how-4@2x.png 2x" alt="" role="presentation"/>
-                            </picture>
-                        </div>
-                        <div>
-                            <template v-if="$i18n.locale === 'en'">
-                                Update your portfolios to <br>maximise profit or avoid losses
-                            </template>
-                            <template v-if="$i18n.locale === 'ru'">
-                                Обновляйте свои портфели, чтобы <br>максимизировать прибыль <br>или избежать убытков
-                            </template>
-                        </div>
-                    </div>
-                    <div class="how-item">
-                        <div class="how-item-img">
-                            <img src="/img/landing/copytrading/how-5-bg.svg" alt="" class="how-item-bg how-item-5">
-                            <picture>
-                                <source srcset="/img/landing/copytrading/how-5.avif 1x, /img/landing/copytrading/how-5@2x.avif 2x" type="image/avif">
-                                <source srcset="/img/landing/copytrading/how-5.webp 1x, /img/landing/copytrading/how-5@2x.webp 2x" type="image/webp">
-                                <img src="/img/landing/copytrading/how-5.png" srcset="/img/landing/copytrading/how-5@2x.png 2x" alt="" role="presentation"/>
-                            </picture>
-                        </div>
-                        <div>
-                            <template v-if="$i18n.locale === 'en'">
-                                Sell portfolio for a profit <br>when the price seems right
-                            </template>
-                            <template v-if="$i18n.locale === 'ru'">
-                                Продайте портфель с прибылью, <br>когда его стоимость увеличилась
-                            </template>
-                        </div>
-                    </div>
-                </div>
                 <template v-if="$i18n.locale === 'en'">
+                    <div class="how-block">
+                        <div class="how-item">
+                            <div class="how-item-img">
+                                <img src="/img/landing/copytrading/how-1-bg.svg" alt="" class="how-item-bg how-item-1">
+                                <picture>
+                                    <source srcset="/img/landing/copytrading/how-1.avif 1x, /img/landing/copytrading/how-1@2x.avif 2x" type="image/avif">
+                                    <source srcset="/img/landing/copytrading/how-1.webp 1x, /img/landing/copytrading/how-1@2x.webp 2x" type="image/webp">
+                                    <img src="/img/landing/copytrading/how-1.png" srcset="/img/landing/copytrading/how-1@2x.png 2x" alt="" role="presentation"/>
+                                </picture>
+                            </div>
+                            <div>Create and top up <br>a wallet in Honee</div>
+                        </div>
+                        <div class="how-item how-item-center">
+                            <div class="how-item-img">
+                                <img src="/img/landing/copytrading/how-2-bg.svg" alt="" class="how-item-bg how-item-2">
+                                <picture>
+                                    <source srcset="/img/landing/copytrading/how-2.avif 1x, /img/landing/copytrading/how-2@2x.avif 2x" type="image/avif">
+                                    <source srcset="/img/landing/copytrading/how-2.webp 1x, /img/landing/copytrading/how-2@2x.webp 2x" type="image/webp">
+                                    <img src="/img/landing/copytrading/how-2.png" srcset="/img/landing/copytrading/how-2@2x.png 2x" alt="" role="presentation"/>
+                                </picture>
+                            </div>
+                            <div>Buy a crypto portfolio <br>managed by experts *</div>
+                        </div>
+                        <div class="how-item">
+                            <div class="how-item-img">
+                                <img src="/img/landing/copytrading/how-3-bg.svg" alt="" class="how-item-bg how-item-3">
+                                <picture>
+                                    <source srcset="/img/landing/copytrading/how-3.avif 1x, /img/landing/copytrading/how-3@2x.avif 2x" type="image/avif">
+                                    <source srcset="/img/landing/copytrading/how-3.webp 1x, /img/landing/copytrading/how-3@2x.webp 2x" type="image/webp">
+                                    <img src="/img/landing/copytrading/how-3.png" srcset="/img/landing/copytrading/how-3@2x.png 2x" alt="" role="presentation"/>
+                                </picture>
+                            </div>
+                            <div>Subscribe to never miss <br>out on portfolio edits</div>
+                        </div>
+                        <div class="how-item how-item-center">
+                            <div class="how-item-img">
+                                <img src="/img/landing/copytrading/how-4-bg.svg" alt="" class="how-item-bg how-item-4">
+                                <picture>
+                                    <source srcset="/img/landing/copytrading/how-4.avif 1x, /img/landing/copytrading/how-4@2x.avif 2x" type="image/avif">
+                                    <source srcset="/img/landing/copytrading/how-4.webp 1x, /img/landing/copytrading/how-4@2x.webp 2x" type="image/webp">
+                                    <img src="/img/landing/copytrading/how-4.png" srcset="/img/landing/copytrading/how-4@2x.png 2x" alt="" role="presentation"/>
+                                </picture>
+                            </div>
+                            <div>Update your portfolios to <br>maximise profit or avoid losses</div>
+                        </div>
+                        <div class="how-item">
+                            <div class="how-item-img">
+                                <img src="/img/landing/copytrading/how-5-bg.svg" alt="" class="how-item-bg how-item-5">
+                                <picture>
+                                    <source srcset="/img/landing/copytrading/how-5.avif 1x, /img/landing/copytrading/how-5@2x.avif 2x" type="image/avif">
+                                    <source srcset="/img/landing/copytrading/how-5.webp 1x, /img/landing/copytrading/how-5@2x.webp 2x" type="image/webp">
+                                    <img src="/img/landing/copytrading/how-5.png" srcset="/img/landing/copytrading/how-5@2x.png 2x" alt="" role="presentation"/>
+                                </picture>
+                            </div>
+                            <div>Sell portfolio for a profit <br>when the price seems right</div>
+                        </div>
+                    </div>
                     <p>* Please read the disclaimer at the bottom before you start any investments in cryptocurrencies.</p>
                 </template>
                 <template v-if="$i18n.locale === 'ru'">
+                    <div class="how-block">
+                        <div class="how-item">
+                            <div class="how-item-img">
+                                <img src="/img/landing/copytrading/how-1-bg.svg" alt="" class="how-item-bg how-item-1">
+                                <picture>
+                                    <source srcset="/img/landing/copytrading/how-1-ru.avif 1x, /img/landing/copytrading/how-1-ru@2x.avif 2x" type="image/avif">
+                                    <source srcset="/img/landing/copytrading/how-1-ru.webp 1x, /img/landing/copytrading/how-1-ru@2x.webp 2x" type="image/webp">
+                                    <img src="/img/landing/copytrading/how-1-ru.png" srcset="/img/landing/copytrading/how-1-ru@2x.png 2x" alt="" role="presentation"/>
+                                </picture>
+                            </div>
+                            <div>Создайте и пополните <br>кошелек в Honee</div>
+                        </div>
+                        <div class="how-item how-item-center">
+                            <div class="how-item-img">
+                                <img src="/img/landing/copytrading/how-2-bg.svg" alt="" class="how-item-bg how-item-2">
+                                <picture>
+                                    <source srcset="/img/landing/copytrading/how-2-ru.avif 1x, /img/landing/copytrading/how-2-ru@2x.avif 2x" type="image/avif">
+                                    <source srcset="/img/landing/copytrading/how-2-ru.webp 1x, /img/landing/copytrading/how-2-ru@2x.webp 2x" type="image/webp">
+                                    <img src="/img/landing/copytrading/how-2-ru.png" srcset="/img/landing/copytrading/how-2-ru@2x.png 2x" alt="" role="presentation"/>
+                                </picture>
+                            </div>
+                            <div>Купите крипто-портфель, <br>управляемый экспертом *</div>
+                        </div>
+                        <div class="how-item">
+                            <div class="how-item-img">
+                                <img src="/img/landing/copytrading/how-3-bg.svg" alt="" class="how-item-bg how-item-3">
+                                <picture>
+                                    <source srcset="/img/landing/copytrading/how-3.avif 1x, /img/landing/copytrading/how-3@2x.avif 2x" type="image/avif">
+                                    <source srcset="/img/landing/copytrading/how-3.webp 1x, /img/landing/copytrading/how-3@2x.webp 2x" type="image/webp">
+                                    <img src="/img/landing/copytrading/how-3.png" srcset="/img/landing/copytrading/how-3@2x.png 2x" alt="" role="presentation"/>
+                                </picture>
+                            </div>
+                            <div>Подпишитесь на уведомления, <br>чтобы не пропустить изменения портфеля</div>
+                        </div>
+                        <div class="how-item how-item-center">
+                            <div class="how-item-img">
+                                <img src="/img/landing/copytrading/how-4-bg.svg" alt="" class="how-item-bg how-item-4">
+                                <picture>
+                                    <source srcset="/img/landing/copytrading/how-4-ru.avif 1x, /img/landing/copytrading/how-4-ru@2x.avif 2x" type="image/avif">
+                                    <source srcset="/img/landing/copytrading/how-4-ru.webp 1x, /img/landing/copytrading/how-4-ru@2x.webp 2x" type="image/webp">
+                                    <img src="/img/landing/copytrading/how-4-ru.png" srcset="/img/landing/copytrading/how-4-ru@2x.png 2x" alt="" role="presentation"/>
+                                </picture>
+                            </div>
+                            <div>Обновляйте свои портфели, чтобы <br>максимизировать прибыль <br>или избежать убытков</div>
+                        </div>
+                        <div class="how-item">
+                            <div class="how-item-img">
+                                <img src="/img/landing/copytrading/how-5-bg.svg" alt="" class="how-item-bg how-item-5">
+                                <picture>
+                                    <source srcset="/img/landing/copytrading/how-5-ru.avif 1x, /img/landing/copytrading/how-5-ru@2x.avif 2x" type="image/avif">
+                                    <source srcset="/img/landing/copytrading/how-5-ru.webp 1x, /img/landing/copytrading/how-5-ru@2x.webp 2x" type="image/webp">
+                                    <img src="/img/landing/copytrading/how-5-ru.png" srcset="/img/landing/copytrading/how-5-ru@2x.png 2x" alt="" role="presentation"/>
+                                </picture>
+                            </div>
+                            <div>Продайте портфель с прибылью, <br>когда его стоимость увеличилась</div>
+                        </div>
+                    </div>
                     <p>* Пожалуйста, прочитайте заявление об отказе от ответственности внизу, прежде чем начинать какие-либо инвестиции в криптовалюты.</p>
                 </template>
             </div>
         </section>
-        <section class="top-portfilios">
+        <!--<section class="top-portfilios">
             <div class="container">
                 <h2>{{ $td('Top Performing Portfolios', 'copytrading.top-portfilios-title')}}</h2>
                 <div class="portfilios-grid">
                     
                 </div>
             </div>
-        </section>
+        </section>-->
     </div>
 </template>
