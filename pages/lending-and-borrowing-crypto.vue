cfdf140d66b4b396afbae47f659f16dcb7923ff6
2z2z2z <61245073+2z2z2z@users.noreply.github.com>
2023-05-04 17:11:11 +0300

    ending-and-borrowing-crypto

---
+++ pages/lending-and-borrowing-crypto.vue
@@ -0,0 +1,96 @@
+<script>
+import getTitle from '~/assets/get-title.js';
+import trackClick from '~/assets/v-track-click.js';
+
+export default {
+    nuxtI18n: {
+        locales: ['en'],
+    },
+    directives: {
+        trackClick,
+    },
+    head() {
+        const title = getTitle('How to lending and borrowing crypto');
+        const description = 'Honee crypto wallet users have the opportunity to lending and borrowing cryptocurrency.';
+
+        return {
+            title: title,
+            meta: [
+                { hid: 'og-title', name: 'og:title', content: title },
+                { hid: 'description', name: 'description', content: description },
+                { hid: 'og-description', name: 'og:description', content: description },
+            ],
+        };
+    },
+    computed: {
+        appUrl() {
+            const localeSuffix = this.$i18n.locale === 'en' ? '' : this.$i18n.locale;
+            return `https://my.honee.app/${localeSuffix}`;
+        },
+    },
+};
+
+</script>
+
+
+<template>
+    <div class="page-seo">
+        <article>
+            <div class="container">
+                <div class="top-line">
+                    <div class="logo">
+                        <a href="/"><img src="/img/logo-honee.svg" alt="Honee" width="122" height="24"/></a>
+                    </div>
+                    <div class="share">
+                        <span>Share</span>
+                        <a href="https://twitter.com/intent/tweet?url=https://honee.app/lending-and-borrowing-crypto&text=How+to+lending+and+borrowing+crypto" target="_blank" rel="nofollow"><img src="/img/landing/twitter.svg" alt="Share on Twitter" width="40" height="40"></a>
+                        <a href="https://www.facebook.com/share.php?u=https://honee.app/lending-and-borrowing-crypto&quote=How+to+lending+and+borrowing+crypto" target="_blank" rel="nofollow"><img src="/img/landing/facebook.svg" alt="Share on Facebook" width="40" height="40"></a>
+                        <a href="tg://msg_url?url=https://honee.app/lending-and-borrowing-crypto&text=How+to+lending+and+borrowing+crypto" rel="nofollow"><img src="/img/landing/telegram.svg" alt="Share on Telegram" width="40" height="40"></a>
+                        <a href="https://www.linkedin.com/sharing/share-offsite/?url=https://honee.app/lending-and-borrowing-crypto" target="_blank" rel="nofollow"><img src="/img/landing/linkedin.svg" alt="Share on Linkedin" width="40" height="40"></a>
+                        <!--<a href="" rel="nofollow"><img src="/img/landing/copy-link.svg" alt="Copy link" width="40" height="40"></a>-->
+                    </div>
+                </div>
+                <section class="article-header">
+                    <h1>How to lending and borrowing crypto</h1>
+                </section>
+                <section>
+                    <h2>Borrowing</h2>
+                    <p>This service lets you quickly get BNB by putting up your BEE tokens as collateral. You can repay BNB within 365 days; the longer the period, the bigger the interest rate. If the BEE you’ve put up decreases in price by 25% or more, the lender may claim it. In such case, you won’t have to repay BNB.</p>
+                    <h3>Example</h3>
+                    <p>You have 10 BEE that is worth 200 BNB. By putting up 10 BEE, you borrow 100 BNB (two times less). Say, in 6 months’ time, you want to repay the loan, so you’ll have to send 106 BNB (100 BNB + 6% accrued over that period) in order to get your 10 BEE back. If the total worth of 10 BEEs and the interest generated went down to 154.5 BNB (-25%), the lender would have the right to keep your BEEs, meaning you wouldn’t have to return 106 BNB anymore.</p>
+                    <h3>Terms</h3>
+                    <ul>
+                        <li>You will receive 50% of pledged BEE value in BNB;</li>
+                        <li>The interest is 12% per annum. 1% is charged once you receive BNB and then continues to charge every 30th day;</li>
+                        <li>You will pay interest at the time of repaying the debt;</li>
+                        <li>If the collateral of the loan (loan + accrued interest) falls below 150%, the pledged amount of BEE may be transferred to the lender;</li>
+                        <li>You can return BNB + % for the period of usage at any time in order to receive your BEE back;</li>
+                        <li>The maximum loan period is 365 days. If by this time, you will not have returned the BNB, then BEE may be transferred to the lender;</li>
+                        <li>The minimum loan amount is $1.</li>
+                    </ul>
+                </section>
+                <section>
+                    <h2>Lending</h2>
+                    <p>This service lets you lend your BNB and earn interest. Before the borrower gets your BNB, they put up their BEE tokens as collateral. If these tokens decrease in price by 25% or more, you may claim them. In such case, the borrower won’t have to repay BNB, while you’ll be able to sell their BEE.</p>
+                    <h3>Example</h3>
+                    <p>You’re lending 100 BNB to the borrower who puts up 10 BEE worth 200 BNB in return (two times the worth of your BNB). Say, in 6 months’ time, the borrower wants to repay the loan, so they send you 106 BNB (100 BNB + 6% accrued over that period) and get their 10 BEE back. If the total worth of 10 BEEs and the interest generated were to go down to 154.5 BNB (-25%), you’d have the right to keep the borrower’s BEEs, while the borrower wouldn’t have to return 106 BNB anymore.</p>
+                    <h3>Terms</h3>
+                    <ul>
+                        <li>You can send BNB and wait for someone to borrow it;</li>
+                        <li>If there is more than 1 lender in the queue, then the funds are used according to the FIFO algorithm;</li>
+                        <li>Unused BNB can be withdrawn at any time;</li>
+                        <li>The interest is 12% per annum. 1% is charged once the borrower receives BNB and then continues to charge every 30th day;</li>
+                        <li>You will receive interest when the debt is repaid;</li>
+                        <li>If the collateral for the used BNB drops below 150%, then you have an opportunity to take the collateral in order to sell it and get your BNB back (10% of the BEE amount goes to Fund);</li>
+                        <li>The minimum amount is $1. The maximum is $10,000.</li>
+                    </ul>
+                </section>
+                <section class="article-last-block">
+                    <div class="cta-center">
+                        <a class="btn btn-2" :href="appUrl" v-track-click="'all-buttons'">{{ $td('Launch Honee', 'index.steps-button')}}</a>
+                    </div>
+                </section>
+            </div>
+        </article>
+    </div>
+</template>
---
+++ pages/ru/lending-and-borrowing-crypto.vue
@@ -0,0 +1,99 @@
+<script>
+import getTitle from '~/assets/get-title.js';
+import trackClick from '~/assets/v-track-click.js';
+
+export default {
+    nuxtI18n: {
+        paths: {
+            ru: '/lending-and-borrowing-crypto',
+        },
+        locales: ['ru'],
+    },
+    directives: {
+        trackClick,
+    },
+    head() {
+        const title = getTitle('Как занять или одолжить криптовалюту');
+        const description = 'Пользователи крипто-кошелька Honee имеют возможность занимать и одалживать криптовалюту.';
+
+        return {
+            title: title,
+            meta: [
+                { hid: 'og-title', name: 'og:title', content: title },
+                { hid: 'description', name: 'description', content: description },
+                { hid: 'og-description', name: 'og:description', content: description },
+            ],
+        };
+    },
+    computed: {
+        appUrl() {
+            const localeSuffix = this.$i18n.locale === 'en' ? '' : this.$i18n.locale;
+            return `https://my.honee.app/${localeSuffix}`;
+        },
+    },
+};
+
+</script>
+
+
+<template>
+    <div class="page-seo">
+        <article>
+            <div class="container">
+                <div class="top-line">
+                    <div class="logo">
+                        <a href="/"><img src="/img/logo-honee.svg" alt="Honee" width="122" height="24"/></a>
+                    </div>
+                    <div class="share">
+                        <span>Поделиться</span>
+                        <a href="https://twitter.com/intent/tweet?url=https://honee.app/ru/lending-and-borrowing-crypto&text=Как+занять+или+одолжить+криптовалюту" target="_blank" rel="nofollow"><img src="/img/landing/twitter.svg" alt="Поделиться в Twitter" width="40" height="40"></a>
+                        <a href="https://www.facebook.com/share.php?u=https://honee.app/ru/lending-and-borrowing-crypto&quote=Как+занять+или+одолжить+криптовалюту" target="_blank" rel="nofollow"><img src="/img/landing/facebook.svg" alt="Поделиться в Facebook" width="40" height="40"></a>
+                        <a href="tg://msg_url?url=https://honee.app/ru/lending-and-borrowing-crypto&text=Как+занять+или+одолжить+криптовалюту" rel="nofollow"><img src="/img/landing/telegram.svg" alt="Поделиться в Telegram" width="40" height="40"></a>
+                        <a href="https://www.linkedin.com/sharing/share-offsite/?url=https://honee.app/ru/lending-and-borrowing-crypto" target="_blank" rel="nofollow"><img src="/img/landing/linkedin.svg" alt="Поделиться в Linkedin" width="40" height="40"></a>
+                        <!--<a href="" rel="nofollow"><img src="/img/landing/copy-link.svg" alt="Скопировать ссылку" width="40" height="40"></a>-->
+                    </div>
+                </div>
+                <section class="article-header">
+                    <h1>Как занять или одолжить криптовалюту</h1>
+                </section>
+                <section>
+                    <h2>Занять</h2>
+                    <p>С помощью этого сервиса вы можете быстро получить BNB, оставив в залог свои токены BEE. Вернуть BNB вы можете в течение года; чем больше срок, тем больше процент переплаты. Если заложенные BEE значительно снижаются в цене (на -25%), то кредитор может забрать их себе. В этом случае вам не понадобится возвращать BNB.</p>
+                    <h3>Пример</h3>
+                    <p>У вас 10 BEE стоимостью 200 BNB. Оставляя 10 BEE в виде залога, вы берете займ в размере 100 BNB (в 2 раза меньше их стоимости). Спустя 6 месяцев вы хотите вернуть займ, для этого отправляете 106 BNB (100 BNB + 6% за 6 месяцев), а назад получаете 10 BEE. Если бы стоимость 10 BEE и начисленных процентов опустилась до 154.5 BNB (-25%)), то кредитор оставил бы BEE себе, при этом 106 BNB возвращать было бы уже не нужно.</p>
+                    <h3>Условия</h3>
+                    <ul>
+                        <li>Вы получите 50% от стоимости BEE в BNB;</li>
+                        <li>Процентная ставка составляет 12% годовых. 1% взимается после получения BNB, а затем продолжает взиматься каждый 30-й день;</li>
+                        <li>Вы заплатите проценты в момент погашения долга;</li>
+                        <li>Если залог кредита (кредит + начисленные проценты) падает ниже 150%, залоговая сумма BEE может быть передана кредитору;</li>
+                        <li>Вы можете вернуть BNB + % за период использования в любое время, чтобы получить свои BEE обратно;</li>
+                        <li>Максимальный срок кредита составляет 365 дней. Если к этому времени вы не вернете BNB, то BEE могут быть переданы кредитору;</li>
+                        <li>Минимальная сумма кредита составляет 1 доллар.</li>
+                    </ul>
+                </section>
+                <section>
+                    <h2>Одолжить</h2>
+                    <p>С помощью этого сервиса вы можете предоставить свои BNB в кредит, чтобы зарабатывать на этом проценты. Когда заемщик получает ваши BNB, он оставляет в залог свои токены BEE. Если заложенные BEE значительно снижаются в цене (на -25%), то вы можете забрать эти токены себе. В этом случае заемщик не будет возвращать BNB, а вы сможете продать оставленные им BEE.</p>
+                    <h3>Пример</h3>
+                    <p>У вас 100 BNB, которые вы предоставляете к займу. Заемщик берет ваши 100 BNB, оставляя 10 BEE стоимостью 200 BNB (в 2 раза больше ваших BNB). Спустя 6 месяцев заемщик хочет вернуть займ, для этого он отправляет вам 106 BNB (100 BNB + 6% за 6 месяцев), а назад получает свои 10 BEE. Если бы стоимость 10 BEE и начисленных процентов опустилась до 154.5 BNB (-25%), то вы вправе оставить BEE себе, при этом 106 BNB остались бы у заемщика.</p>
+                    <h3>Условия</h3>
+                    <ul>
+                        <li>Вы можете отправить BNB и дождаться, пока кто-нибудь возьмет их взаймы;</li>
+                        <li>Если в очереди более 1 кредитора, то средства используются по алгоритму FIFO;</li>
+                        <li>Неиспользованные BNB можно вывести в любое время;</li>
+                        <li>Процентная ставка составляет 12% годовых. 1% взимается, как только заемщик получает BNB, а затем продолжает взиматься каждый 30-й день;</li>
+                        <li>Вы получите проценты, когда долг будет погашен;</li>
+                        <li>Если залог за использованный BNB падает ниже 150%, то у вас есть возможность забрать залог, чтобы продать его и вернуть свой BNB (10% от суммы BEE идет в фонд);</li>
+                        <li>Минимальная сумма составляет $1. Максимум $10 000.</li>
+                    </ul>
+                </section>
+                <section class="article-last-block">
+                    <div class="cta-center">
+                        <a class="btn btn-2" :href="appUrl" v-track-click="'all-buttons'">{{ $td('Launch Honee', 'index.steps-button')}}</a>
+                    </div>
+                </section>
+            </div>
+        </article>
+    </div>
+</template>
